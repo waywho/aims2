309e96137ae5abba281a26de3ecc0bb688aef594
WeiHsi Hu <weihsi.hu@gmail.com>
2015-12-07 15:57:45 +0000

    add secondary menu and styled Summer Course page

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,7 +1,18 @@
 class CoursesController < ApplicationController
+	before_action :set_courses
 
   def index
     @courses = Course.all
+  end
+
+  def show
+  	@course = Course.find(params[:id])
+  end
+
+  private
+
+  def set_courses
+  	@courses = Course.all
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 		resources :courses
 	end
 
+  resources :courses
+
 
   root 'courses#index'
   # The priority is based upon order of creation: first created -> highest priority.
