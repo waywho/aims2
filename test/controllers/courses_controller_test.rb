78e4d5e9537204289c1e7e900da286867fd986cc
WeiHsi Hu <weihsi.hu@gmail.com>
2015-12-07 23:31:38 +0000

    add test

--- test/controllers/courses_controller_test.rb
+++ test/controllers/courses_controller_test.rb
@@ -4,4 +4,20 @@ class CoursesControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  setup do
+  	@course = Course.create(:name => 'Solo', :description => 'This is a great course.')
+  end
+
+  test "should get index" do
+  	get :index
+  	assert_response :success
+  	assert_not_nil assigns(:courses)
+  end
+
+  test "show course" do
+  	get :show, :id => @course.id
+  	assert_response :success
+  end
+
 end
