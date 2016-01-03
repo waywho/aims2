864338057375ceff5cf331bc24f76de92ae33351
WeiHsi <weihsi.hu@gmail.com>
2016-01-03 22:20:25 +0000

    add casein fees controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
 
 	#Casein routes
 	namespace :casein do
+		resources :fees do
+      collection do
+        post :edit_multiple
+        post :update_multiple
+      end
+    end
 		resources :events do
       collection do
         post :edit_multiple
