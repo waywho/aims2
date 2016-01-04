3a6eb7e1ac4dfcf4732c2dd8518c5a47bbb34886
WeiHsi Hu <weihsi.hu@gmail.com>
2016-01-04 15:36:21 +0000

    remove published_at

--- db/migrate/20151230133403_remove_columns.rb
+++
@@ -1,8 +0,0 @@
-class RemoveColumns < ActiveRecord::Migration
-  def change
-  	remove_column :klasses, :published_at
-  	remove_column :courses, :published_at
-  	remove_column :pages, :published_at
-  	remove_column :staffs, :published_at
-  end
-end
