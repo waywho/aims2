2f2ca5a03bb1084a2b83e173bf8cb1a0802200e5
WeiHsi Hu <weihsi.hu@gmail.com>
2016-01-04 14:55:33 +0000

    remove format migrations

--- db/migrate/20151217180652_add_column_to_formats.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnToFormats < ActiveRecord::Migration
-  def change
-    add_column :formats, :slug, :string
-    add_index :formats, :slug
-  end
-end
--- db/migrate/20151224102155_add_columns_to_format.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnsToFormat < ActiveRecord::Migration
-  def change
-    add_column :formats, :published_at, :timestamp
-    add_column :formats, :workflow_state, :string
-  end
-end
