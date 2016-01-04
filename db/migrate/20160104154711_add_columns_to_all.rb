2103f184dd418b53f2e38502d7db6cdd07f0d2c5
WeiHsi Hu <weihsi.hu@gmail.com>
2016-01-04 22:28:12 +0000

    change migration for heroku

--- db/migrate/20160104154711_add_columns_to_all.rb
+++ db/migrate/20160104154711_add_columns_to_all.rb
@@ -4,7 +4,6 @@ class AddColumnsToAll < ActiveRecord::Migration
   	add_column :courses, :published_at, :datetime
   	add_column :events, :published_at, :datetime
   	add_column :fees, :published_at, :datetime
-  	add_column :klasses, :published_at, :datetime
   	add_column :pages, :published_at, :datetime
   	add_column :staffs, :published_at, :datetime
   	add_column :quotes, :published_at, :datetime
