b97ab5fc84daaddecf02603a926cf80155508182
WeiHsi Hu <weihsi.hu@gmail.com>
2016-01-13 22:13:44 +0000

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,24 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Casein::AdminUser.delete_all
-Casein::AdminUser.create({login: "admin", email:"walzerfan@yahoo.com", password:"password"})
+Courseformat.delete_all
+format_summer = Courseformat.create({title: "Summer School 2016", description: "The Summer School is a week-long residential course at the beautiful seaside of East Bourne. The You will be joining around 200 enthusiastic students, along with 70 staff made up of tutors and house pianists. There four main teaching sessions per day, with lots of performance opportunities.", whats_new: "2016 is very new", when_from: "2016-08-21 08:53:20", when_to: "2016-08-28 21:53:20", venue: "EastBourne College", address1: "Old Wish Road", city: "EastBourne", county: "East Sussex", post_code: "BN21 4JY", workflow_state: "published", homepage_feature: true})
+format_mini = Courseformat.create({title: "Mini-Aims 2016", description: "Mini-Aims is our non-residential 'taster' version of the main singing summer school.", whats_new: "2016 is very new", when_from: "2016-04-09 08:53:20", when_to: "2016-04-10 21:53:20", venue: "St Gabriel's Halls", address1: "Pimlico", city: "London", county: "Greater London", post_code: "SW1V 3AA", workflow_state: "published"})
+format_summer.highlights.create([{title: "Gala Concert", description: "Stephan Loges, bass-baritone", workflow_state: "published"},
+	{title: "Celebrity Masterclass", description: "Kathryn Harris, soprano and Director of National Opera Studio", workflow_state: "published"},
+	{title: "AIMS Choir", description: "Nicholas Jenkins, conductor"}, 
+	{title: "Staged Opera Scenes", description: "Operas of Shakespeare plays"}, 
+	{title: "Small Vocal Ensembles", description: "Shakespeare part-song settings and Madrigals"}, 
+	{title: "Early Music Scenes", description: "The Fairy Queen, Purcell"}, 
+	{title: "The Lighter Side", description: "The Yeomen of the Guard, Kissing Me Kate, and West Side Story"}])
 
 Course.delete_all
-course_solo = Course.create({title: "Solo Course", description: "Our SOLO course is aimed at singers of all ages (18 plus). This could be young professionals on the threshold of a career, students at music colleges, amateurs of 'semi-pro' standard and singers with a little less experience, but a desire to sing really well."})
-course_choral = Course.create({title: "Choral Course", description: "Our CHORAL course is open to all. We offer a range of choirs and vocal groups, covering a varied repertoire, catering, we hope, to individual tastes. Crucially, all our choral classes culminate in performances, be it lunctime concerts, or larger choral works with soloists and orchestra."})
-course_cross = Course.create({title: "CROSSOVER Course", description: "The CROSSOVER COURSE has become increasingly popular in recent years, offers studetns the chance to 'pick and choose' from the SOLO or CHORAL courses. We feel this gives people a chance to really tailor their week with us, according to their preferences."})
-course_piano = Course.create({title: "Piano Accompanist Course", description: "This is a great course for established duos, or for pianists looking to progress. This course is very much tailored to individual needs: you may be following a particular singer throughout their week, or you may be partnered with different singers, according to your preference and areas of interest. The aim is to produce and individual learning and performance opportunities with some of the best accompanist tutors."})
-course_other = Course.create({title: "Other Course", description: "New in 2016, we have space for up to six students to take choral conducting classes with Gregory Rose. Students will need to be enrolled as either a choral or a crossover singing student, and alongside singing classes, they will work with Gregory on stick technique, observing and conducting."})
-course_dropin = Course.create({title: "Drop-In Course", description: "Open to all students, by signing up in the onsite AIMS Office."})
+course_solo = format_summer.courses.create({title: "Solo Course", description: "Our SOLO course is aimed at singers of all ages (18 plus). This could be young professionals on the threshold of a career, students at music colleges, amateurs of 'semi-pro' standard and singers with a little less experience, but a desire to sing really well.", workflow_state: "published"})
+course_choral = format_summer.courses.create({title: "Choral Course", description: "Our CHORAL course is open to all. We offer a range of choirs and vocal groups, covering a varied repertoire, catering, we hope, to individual tastes. Crucially, all our choral classes culminate in performances, be it lunctime concerts, or larger choral works with soloists and orchestra.", workflow_state: "published"})
+course_cross = format_summer.courses.create({title: "CROSSOVER Course", description: "The CROSSOVER COURSE has become increasingly popular in recent years, offers studetns the chance to 'pick and choose' from the SOLO or CHORAL courses. We feel this gives people a chance to really tailor their week with us, according to their preferences.", workflow_state: "published"})
+course_piano = format_summer.courses.create({title: "Piano Accompanist Course", description: "This is a great course for established duos, or for pianists looking to progress. This course is very much tailored to individual needs: you may be following a particular singer throughout their week, or you may be partnered with different singers, according to your preference and areas of interest. The aim is to produce and individual learning and performance opportunities with some of the best accompanist tutors."})
+course_other = format_summer.courses.create({title: "Other Course", description: "New in 2016, we have space for up to six students to take choral conducting classes with Gregory Rose. Students will need to be enrolled as either a choral or a crossover singing student, and alongside singing classes, they will work with Gregory on stick technique, observing and conducting.", workflow_state: "published"})
+course_dropin = format_summer.courses.create({title: "Drop-In Course", description: "Open to all students, by signing up in the onsite AIMS Office."})
 
 Klass.delete_all
 course_solo.klasses.create({title: "Vocal Technique", description: "Offered to all solo singers, this covers two sessions in the week, with the same tutor. Classes will focus on the technical aspects of singing and breathing. Bring any music that you are working on.", number_of_sessions: 2})
@@ -26,19 +34,8 @@ course_choral.klasses.create({title: "Madrigals", description: "This course for 
 course_choral.klasses.create({title: "Oratorio Choir", description: "Mendelssohn Christus and Psalm 13 new editions by Neil Jenkins, published Barn End Press.", repertoire: "Mendelssohn Christus and Psalm 13, new editions by Neil Jenkins, published Barns End Press. Mendelssohn Hear my prayer, published by Novello, NOV291599", number_of_sessions: 6, session_of_day: 'Session 2'})
 course_choral.klasses.create({title: "Compline Choir", description: "This choir will rehearse the music for a service of Compline on the Wednesday evening. The Compline service at AIMS is a wonderful midpoint of the week. It is a chance to take some time out from the rus, and enjoy some beautiful sacred works, both accompanied and a capella.", repertoire: "Music for the Funeral of Queen Mary, Purcell, Burial Sentences, Croft, Nunc Dimittis, Howell, and motets from Elizabethan/Tudor period.", session_of_day: "Session 3", number_of_sessions: 3})
 course_choral.klasses.create({title: "AIMS Choir", description: "All choral students are encouraged to sing in the AIMS Choir", repertoire: "Haydn The Seasons (Autumn & Winter) published by Barn End Press", session_of_day: 'Session 4', number_of_sessions: '6'})
-
 course_dropin.klasses.create({title: "Stagecraft", description: "Taken by our Opera Directors, these classes cover various aspects of acting technique. They are lively, with lots of participation for all. Specific themes will be covered"})
 
-Courseformat.delete_all
-format_summer = Courseformat.create({title: "Summer School 2016", description: "The Summer School is a week-long residential course at the beautiful seaside of East Bourne. The You will be joining around 200 enthusiastic students, along with 70 staff made up of tutors and house pianists. There four main teaching sessions per day, with lots of performance opportunities.", whats_new: "2016 is very new", when_from: "2016-08-21 08:53:20", when_to: "2016-08-28 21:53:20", venue: "EastBourne College", address1: "Old Wish Road", city: "EastBourne", county: "East Sussex", post_code: "BN21 4JY"})
-format_mini = Courseformat.create({title: "Mini-Aims 2016", description: "Mini-Aims is our non-residential 'taster' version of the main singing summer school.", whats_new: "2016 is very new", when_from: "2016-04-09 08:53:20", when_to: "2016-04-10 21:53:20", venue: "St Gabriel's Halls", address1: "Pimlico", city: "London", county: "Greater London", post_code: "SW1V 3AA"})
-format_summer.highlights.create([{title: "Gala Concert", description: "Stephan Loges, bass-baritone"},
-	{title: "Celebrity Masterclass", description: "Kathryn Harris, soprano and Director of National Opera Studio"},
-	{title: "AIMS Choir", description: "Nicholas Jenkins, conductor"}, 
-	{title: "Staged Opera Scenes", description: "Operas of Shakespeare plays"}, 
-	{title: "Small Vocal Ensembles", description: "Shakespeare part-song settings and Madrigals"}, 
-	{title: "Early Music Scenes", description: "The Fairy Queen, Purcell"}, 
-	{title: "The Lighter Side", description: "The Yeomen of the Guard, Kissing Me Kate, and West Side Story"}])
 
 Staff.delete_all
 Staff.create({name: "John Ramster", biography: "Opera Director at the Royal Academy of Music", role: "tutor"})
@@ -60,8 +57,8 @@ Event.create({title: "Gala Recital", description: "Some amazing singer.", date: 
 Page.delete_all
 Page.create({title: "Ticket Information", content: "You can buy tickets at the door"})
 Page.create({title: "Accommodation", content: "You will live here"})
-Page.create({title: "Contact Us", content: "I will call you"})
-Page.create({title: "About Us", content: "We are just brilliant"})
+Page.create({title: "Contact Us", content: "I will call you", workflow_state: "published"})
+Page.create({title: "About Us", content: "We are just brilliant", workflow_state: "published"})
 Page.create({title: "Support Us", content: "Your support is very helpful"})
 Page.create({title: "What Happens Next?", content: "You will receive a booking confirmation"})
 Page.create({title: "Location", content: "The course takes place on the lovely campus of Eastbourne College"})
@@ -70,6 +67,7 @@ Page.create({title: "Auditions", content: "This is how you audition"})
 Page.create({title: "A Typical Day", content: "This is the typical day"})
 Page.create({title: "Staff", content: "Here are our staff"})
 Page.create({title: "Course Information", content: "courses info"})
+Page.create({title: "Welcome", content: "This is our 10th year!", workflow_state: "published", feature_page: true})
 NewsItem.create({title: "Great News", description: "There is a new news."})
 NewsItem.create({title: "Wonderful News", description: "My news is great."})
 NewsItem.create({title: "Expectations", description: "Another wonderful life."})
@@ -95,7 +93,7 @@ Fee.create({fee_type: "Event", category: "Concert", description: "Celebrity Mast
 Fee.create({fee_type: "Event", category: "Concert", description: "Lunchtime Concerts", amount: "0"})
 
 Quote.delete_all
-Quote.create({saying: "Fabulous week at AIMS but then it always is! Lovely to see old friends and meet new ones, and share in this life-enhancing musical experience."})
-Quote.create({saying: "As the week progressed, it was nice to know we all shared the same vocal issues."})
+Quote.create({saying: "Fabulous week at AIMS but then it always is! Lovely to see old friends and meet new ones, and share in this life-enhancing musical experience.", workflow_state: "published"})
+Quote.create({saying: "As the week progressed, it was nice to know we all shared the same vocal issues.", workflow_state: "published"})
 
 
