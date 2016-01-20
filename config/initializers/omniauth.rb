995ac2d71fa009c898af955f3cbb818cc2e5550b
WeiHsi Hu <weihsi.hu@gmail.com>
2016-01-20 10:51:25 +0000

    remove oauth method

--- config/initializers/omniauth.rb
+++
@@ -1,5 +0,0 @@
-OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :salesforce, 'YOUR-CONSUMER-KEY', 'YOUR-CONSUMER-SECRET'
-end
--- config/initializers/restforce.rb
+++ config/initializers/restforce.rb
@@ -1,7 +1,8 @@
 Restforce.configure do |config|
-  config.client_id = "3MVG92u_V3UMpV.hdaH4EpiKBPi7wCzIVDIWlwmaIvzWA8N_bmQ1O4DUD8FDNzYqtplH0g082GmoV5skSAti1"
-  config.client_secret = "335148879781836643"
-  config.host = 'https://test.salesforce.com'
-  config.api_version = '29.0'
-
+	config.username = 'webdev@aims.uk.com.webdev'
+	config.password = 'Greatadmin1'
+  	config.client_id = "3MVG92u_V3UMpV.hdaH4EpiKBPi7wCzIVDIWlwmaIvzWA8N_bmQ1O4DUD8FDNzYqtplH0g082GmoV5skSAti1"
+  	config.client_secret = "335148879781836643"
+  	config.host = 'test.salesforce.com'
+  	config.api_version = '32.0'
 end
