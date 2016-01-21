cb58932c80f96e232e47b70306c6bc4b68edfd25
WeiHsi Hu <weihsi.hu@gmail.com>
2016-01-21 10:53:41 +0000

    play with the methods

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,9 +1,23 @@
 class BookingsController < ApplicationController
+	before_action :load_client
+
   def index
-  	@client = Restforce.new
-  	@bookings = @client.query('select Name from Opportunity')
+  	# @bookings = @client.query('select Account, Name from Opportunity')
+  	@bookings = @client.describe_layouts('Opportunity')
+
+  	# @choral = @client.picklist_values('Opportunity', 'Choral_Classes_c')
   end
 
   def show
+  end
+
+  def create
+  end
+
+  private
+
+  def load_client
+  	@client ||= Restforce.new(:oauth_token => Rails.cache.read('salesforceforce_oauth_token'))
+  	Rails.cache.write('salesforceforce_oauth_token', @client.options[:oauth_token])
   end
 end
