aa2e65d94b9071fe9500f55c9dd3e71b9c7ebb11
WeiHsi Hu <weihsi.hu@gmail.com>
2016-01-26 23:38:12 +0000

    add booking form fields

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,16 +2,21 @@ class BookingsController < ApplicationController
 	before_action :load_client
 
   def index
-  	# @bookings = @client.query('select Account, Name from Opportunity')
-  	@bookings = @client.describe_layouts('Opportunity')
+  	@bookings = @client.query('select Amount, CloseDate, Name, Campaign_Name__c, Course__c from Opportunity')
+  	@accounts = @client.query('select Name from Account')
+  	# @bookings = @client.describe('Opportunity')
 
-  	# @choral = @client.picklist_values('Opportunity', 'Choral_Classes_c')
+  	@choral_classes = @client.picklist_values('Opportunity', 'Choral_Classes__c')
+  	@sf_courses = @client.picklist_values('Opportunity', 'Course__c')
+  	@ensemble_classes = @client.picklist_values('Opportunity', 'Ensemble_Classes__c')
+
   end
 
   def show
   end
 
   def create
+  	# @client.create('Account')
   end
 
   private
@@ -19,5 +24,9 @@ class BookingsController < ApplicationController
   def load_client
   	@client ||= Restforce.new(:oauth_token => Rails.cache.read('salesforceforce_oauth_token'))
   	Rails.cache.write('salesforceforce_oauth_token', @client.options[:oauth_token])
+  end
+
+  def course_params
+    params.require(:booking).permit(:name, :recordtype, :phone, :billingaddress, :email, :firstName, :lastName)
   end
 end
