ed3cca6c8946febc594b7c152100a896bce43686
WeiHsi Hu <weihsi.hu@gmail.com>
2016-02-02 00:13:02 +0000

    booking form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,9 +2,9 @@ class BookingsController < ApplicationController
 	before_action :load_client
 
   def index
-  	@bookings = @client.query('select Amount, CloseDate, Name, Campaign_Name__c, Course__c from Opportunity')
-  	@accounts = @client.query('select Name from Account')
-  	# @bookings = @client.describe('Opportunity')
+  	# @bookings = @client.query('select Amount, CloseDate, Name, Campaign_Name__c, Course__c from Opportunity')
+  	# @accounts = @client.query('select Name from Account')
+  	@booking = @client.describe('Opportunity')
 
   	@sf_courses = @client.picklist_values('Opportunity', 'Course__c')
     @solo_classes = @client.picklist_values('Opportunity', 'Solo_classes__c')
--- app/helpers/bookings_helper.rb
+++ app/helpers/bookings_helper.rb
@@ -1,2 +1,20 @@
 module BookingsHelper
+	def sf_session_options(fields, selection, controllingField, client)
+		field = fields.find { |f| f['controllerName'] == controllingField }
+		if field.present?
+ 			client.picklist_values('Opportunity', field['name'], :valid_for => selection)
+ 		end
+ 	end
+
+ 	def sf_session2_options(client, selection)
+ 		client.picklist_values('Opportunity', 'Session_2_Options__c', :valid_for => selection)
+ 	end
+
+ 	def sf_session3_options(client, selection)
+ 		client.picklist_values('Opportunity', 'Session_3_Options__c', :valid_for => selection)
+ 	end
+
+ 	def sf_session4_options(client, selection)
+ 		client.picklist_values('Opportunity', 'Session_4_Options__c', :valid_for => selection)
+ 	end
 end
