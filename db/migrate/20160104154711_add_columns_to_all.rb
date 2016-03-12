d6a4de95b0683e25f3048cf80cdf46a662930d9f
WeiHsi Hu <weihsi.hu@gmail.com>
2016-03-12 02:58:11 +0000

    change migration file

--- db/migrate/20160104154711_add_columns_to_all.rb
+++ db/migrate/20160104154711_add_columns_to_all.rb
@@ -1,6 +1,5 @@
 class AddColumnsToAll < ActiveRecord::Migration
   def change
-  	add_column :course_formats, :published_at, :datetime
   	add_column :courses, :published_at, :datetime
   	add_column :events, :published_at, :datetime
   	add_column :fees, :published_at, :datetime
